f47545b0e2f223eb6e0cfc55a1751c1dd1ce48a0
Gustavo <gugacorchog@gmail.com>
2022-05-05 20:33:38 +1000

    Updates

---
+++ precache-manifest.2ce084deea443926c49981d14b789995.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f4dfaa960ecac306536d6aa433ea171",
+    "url": "/swipejobs/index.html"
+  },
+  {
+    "revision": "deeaa65478aba1055c4c",
+    "url": "/swipejobs/static/css/2.3661fc73.chunk.css"
+  },
+  {
+    "revision": "40d2775f6134c448e0e9",
+    "url": "/swipejobs/static/css/main.e6058d04.chunk.css"
+  },
+  {
+    "revision": "deeaa65478aba1055c4c",
+    "url": "/swipejobs/static/js/2.a1c3bd4e.chunk.js"
+  },
+  {
+    "revision": "40c188938ba4fe18dad43129f97bc71e",
+    "url": "/swipejobs/static/js/2.a1c3bd4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40d2775f6134c448e0e9",
+    "url": "/swipejobs/static/js/main.2c3e0774.chunk.js"
+  },
+  {
+    "revision": "75bc4909e2eb69f2c6e0",
+    "url": "/swipejobs/static/js/runtime-main.8ba99713.js"
+  }
+]);
